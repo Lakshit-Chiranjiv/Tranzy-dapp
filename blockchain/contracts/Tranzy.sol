5f6152dc1011380d39036db77661bc00f55f5b48
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-18 13:02:13 +0530

    event transfer

---
+++ blockchain/contracts/Tranzy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Tranzy {
+
+    uint transactionCount;
+
+    event Transfer(address from, address to, uint amount, string message, uint timestamp);
+
+    
+
+}
