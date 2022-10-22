748582cb4339c8e376113fa68431188a3fb25eb8
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-10-23 01:03:12 +0530

    navbar structure

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,23 @@
+<script>
+    import tranzyLogo from './../assets/tranzyLogo.png'
+</script>
+
+<main>
+    <nav class="nav">
+        <img src={tranzyLogo} alt="logo">
+        <h3>Hello User</h3>
+    </nav>
+</main>
+
+<style>
+    .nav{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        gap: 30px;
+    }
+
+    .nav img{
+        width: 360px;
+    }
+</style>
